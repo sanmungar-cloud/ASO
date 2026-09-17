e21326b8f3d9aa524b17fbe9bb7286de9090b336
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-17 09:26:40 +0200

    Habia declarado por error 'por' con dolar delante por lo que me fallaba todo el rato el programa

--- bash/sesion1_repaso/nivel_disco.sh
+++ bash/sesion1_repaso/nivel_disco.sh
@@ -1,11 +1,9 @@
-$por = $(( ($1 * 100) / $2 ))
-case $por in
-    0-70)
-        echo "OK"
-        ;;
-    71-89)
-        echo "AVISO"
-        ;;
-    90-100)
-        echo "CRITICO"
-        ;;
+por=$(( ($1 * 100) / $2 ))
+
+if [ $por -lt 70 ]; then
+    echo "OK"
+elif [ $por -ge 70 ] && [ $por -lt 90 ]; then
+    echo "AVISO: El disco está entre el 70% y el 89% lleno."
+else
+    echo "CRÍTICO"
+fi
