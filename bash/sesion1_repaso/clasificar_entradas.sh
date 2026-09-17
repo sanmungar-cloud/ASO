d360d1501db039f1a1e46ff59df926e5d49fea81
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-17 09:35:18 +0200

    Ejercicio de comprobar hecho

--- bash/sesion1_repaso/clasificar_entradas.sh
+++ bash/sesion1_repaso/clasificar_entradas.sh
@@ -1,12 +1,10 @@
 for entrada in ~/prueba_bash/*; do
     nombre=$(basename "$entrada")
-    
+    echo "-------"
     if [ -d "$entrada" ]; then
-        echo "$nombre  es un directorio"
-        echo "n/"
+        echo "$nombre  es un directorio."
         
     elif [ -f "$entrada" ]; then
-        echo "$nombre  es un fichero"
-        echo "n/"   
+        echo "$nombre  es un fichero."
     fi
 done
