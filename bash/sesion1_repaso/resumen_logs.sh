16a45b8ae0746c5ca458417054f3699e5a7bf917
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-17 10:20:21 +0200

    No condsigo hacer el ultimo

--- bash/sesion1_repaso/resumen_logs.sh
+++ bash/sesion1_repaso/resumen_logs.sh
@@ -1,5 +1,7 @@
-if [ -d "$1" ]; then
-    find ~/prueba_bash -type f -name "*.log"
+nombre=$(basename "$1")
+
+if [ -d "$nombre" ]; then
+    find /home/sanmungar/Documentos/ASO/bash/sesion1_repaso -type f -name "*.log" ls -l | sort -k5 -n | tail -n 10
 else
     echo "El argumento proporcionado no es un directorio."
 fi 
