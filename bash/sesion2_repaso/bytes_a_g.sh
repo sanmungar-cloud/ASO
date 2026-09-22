14b85d476687516c585f3eaf490957db61cd0d83
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-22 13:48:41 +0200

    Hago un ejemplo de prueba para este ejercicio

--- bash/sesion2_repaso/bytes_a_g.sh
+++ bash/sesion2_repaso/bytes_a_g.sh
@@ -2,3 +2,9 @@ bytes_a_gb() {
     local bytes=$1
     echo "scale=2; $bytes / 1073741824" | bc
 }
+
+echo -n "bytes_a_gb 5000000000 -> "
+bytes_a_gb 5000000000
+
+echo -n "bytes_a_gb 1073741824 -> "
+bytes_a_gb 1073741824
