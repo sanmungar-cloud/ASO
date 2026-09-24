5f800a69874c5c87ecd6758a0bff69af2f095483
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-24 08:30:44 +0200

    Hago la primera prueba de clasificar http

--- bash/sesion3_repaso/clasificar_http.sh
+++ bash/sesion3_repaso/clasificar_http.sh
@@ -5,8 +5,11 @@ if [[ $codigo_http -ge 200 ]] && [[ $codigo_http -le 299 ]]; then
 elif [[ $codigo_http -ge 300 ]] && [[ $codigo_http -le 399 ]]; then
     echo "Redirección"
 elif [[ $codigo_http -ge 400 ]] && [[ $codigo_http -le 499 ]]; then
+    echo "Error del cliente"
+elif [[ $codigo_http -ge 500 ]] && [[ $codigo_http -le 599 ]]
     echo "Error del servidor"
-elif [[  ]] && 
+else
+    echo "No has introducido una URL adecuada."
 
 
 
