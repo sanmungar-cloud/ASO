dfcdbc8ce41236d9836b76a8b020c5ebdbe97999
Santiago Muñoz <santiagomg007@gmail.com>
2026-09-24 08:35:59 +0200

    Clasificar _http hecho

--- bash/sesion3_repaso/clasificar_http.sh
+++ bash/sesion3_repaso/clasificar_http.sh
@@ -1,15 +1,15 @@
-read -p "Ingresa tu código HTTP: " -r codigo_http
 
-if [[ $codigo_http -ge 200 ]] && [[ $codigo_http -le 299 ]]; then
+if [[ $1 -ge 200 ]] && [[ $1 -le 299 ]]; then
     echo "Éxito"
-elif [[ $codigo_http -ge 300 ]] && [[ $codigo_http -le 399 ]]; then
+elif [[ $1 -ge 300 ]] && [[ $1 -le 399 ]]; then
     echo "Redirección"
-elif [[ $codigo_http -ge 400 ]] && [[ $codigo_http -le 499 ]]; then
+elif [[ $1 -ge 400 ]] && [[ $1 -le 499 ]]; then
     echo "Error del cliente"
-elif [[ $codigo_http -ge 500 ]] && [[ $codigo_http -le 599 ]]
+elif [[ $1 -ge 500 ]] && [[ $1 -le 599 ]]; then
     echo "Error del servidor"
 else
-    echo "No has introducido una URL adecuada."
+    echo "No has introducido un código adecuado."
+fi
 
 
 
